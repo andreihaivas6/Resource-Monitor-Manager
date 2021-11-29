274016de8c11dffce58944b1fd232e563a926347
andreihaivas6 <andreihaivas006@gmail.com>
2021-11-29 12:28:38 +0200

    Collect CPU Resources

---
+++ system_resources/cpu_resources.py
@@ -0,0 +1,21 @@
+import psutil
+
+
+class CPUResources:
+    INTERVAL_CPU = 0.5
+
+    def __init__(self):
+        cpu_percents = psutil.cpu_times_percent(interval=CPUResources.INTERVAL_CPU)
+        self._cpu_percent = 100 - cpu_percents.idle
+        self._cpu_percent_user = cpu_percents.user
+        self._cpu_percent_system = cpu_percents.system
+
+        self._processes_count = len(psutil.pids())
+        self._threads_count = sum([
+            process.num_threads()
+            for process in psutil.process_iter()
+        ])
+
+        self._cpu_logical_count = psutil.cpu_count()
+        self._cpu_physical_count = psutil.cpu_count(logical=False)
+        self._cpu_base_speed = psutil.cpu_freq().current
